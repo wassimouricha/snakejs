2422ef028282ec4b05d9cdf0d53c7c524f845879
wassimouricha <wbouricha5@gmail.com>
2022-10-01 13:18:25 +0200

    ajout de la tete du serpent et de la nourriture du serpent dans le canvas

--- assets/script/snake.js
+++ assets/script/snake.js
@@ -1,10 +1,22 @@
-// tableau/planche
+// la majorité du script va se abser sur le systme de canvas
+
+// tableau/planche 
 
 var blockSize = 25;
 var rows = 20;
 var columns = 20;
 var board;
 var context;
+
+// tete du serpent
+// j'indique que le serpent va commencer 5 cases sur l'axe X et 5 cases sur l'axe Y 
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// nourriture du serpent
+
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
 
 // au chargement de la page 
 
@@ -23,4 +35,10 @@ window.onload = function() {
 function update() {
     context.fillStyle="black"; //je spécifie la couleur qui sera utilisé à l'intérieur du canvas
     context.fillRect(0, 0, board.width, board.height); //on remplis du coin du canvas jusqu'a l'autre extrémité
+
+    context.fillStyle="lime"; //couleur du serpent
+    context.fillRect(snakeX,snakeY , blockSize, blockSize); //coin gauche et droit puis la hauteur et la largeur du serpent
+
+    context.fillStyle="red"; //couleur du serpent
+    context.fillRect(foodX,foodY , blockSize, blockSize); //coin gauche et droit puis la hauteur et la largeur du serpent
 }
